281c7f8ce14167976ccb8d69482a46c6ef1f3d01
riccardodivirgilio <riccardodivirgilio@gmail.com>
2024-04-29 16:48:52 +0200

    code lint

--- PythonFunctions/Python/Common/function_handler.py
+++ PythonFunctions/Python/Common/function_handler.py
@@ -1,6 +1,8 @@
-from wolframclient.utils.importutils import import_string
-from wolframclient.language import wl
 from functools import partial
+
+from wolframclient.language import wl
+from wolframclient.utils.importutils import import_string
+
 
 def run_validation(func, *args, **opts):
 
@@ -12,9 +14,9 @@ def run_validation(func, *args, **opts):
 
         errors = exception.errors()
 
-        if len(errors) == 1 and errors[0]['loc'] == (0, ):
+        if len(errors) == 1 and errors[0]["loc"] == (0,):
 
-            if errors[0]['type'] == 'list_type':
+            if errors[0]["type"] == "list_type":
 
                 if isinstance(args[0], dict):
 
@@ -22,15 +24,12 @@ def run_validation(func, *args, **opts):
 
                 if not isinstance(args[0], (list, tuple)):
 
-                    return run_validation(func, (args[0], ), *args[1:], **opts)
+                    return run_validation(func, (args[0],), *args[1:], **opts)
 
             else:
 
                 if isinstance(args[0], (tuple, list)):
-                    return tuple(
-                        run_validation(func, p, *args[1:], **opts)
-                        for p in args[0]
-                    )
+                    return tuple(run_validation(func, p, *args[1:], **opts) for p in args[0])
 
                 if isinstance(args[0], dict):
                     return {
@@ -38,32 +37,27 @@ def run_validation(func, *args, **opts):
                         for k, p in args[0].items()
                     }
 
-                raise NotImplementedError('Should never happen 😅')
-
-
+                raise NotImplementedError("Should never happen 😅")
 
                 # anything else is a valid error
         #
 
         return wl.Failure(
-            "InterpretationFailure", {
+            "InterpretationFailure",
+            {
                 #'MessageTemplate': "\n".join(err['msg'] for err in errors),
-                'MessageTemplate': str(exception),
-                'MessageParameters': {},
-                'Errors': tuple(
+                "MessageTemplate": str(exception),
+                "MessageParameters": {},
+                "Errors": tuple(
                     {
-                        'Type': err['type'],
-                        'Message': err['msg'],
-                        'Path': tuple(
-                            i + 1 if isinstance(i, int) else i
-                            for i in err['loc']
-                        ),
+                        "Type": err["type"],
+                        "Message": err["msg"],
+                        "Path": tuple(i + 1 if isinstance(i, int) else i for i in err["loc"]),
                     }
                     for err in errors
-                )
-            }
+                ),
+            },
         )
-
 
 
 def function_handler(path, validate_call=True):
