ada56165b82e54cb4a14578102433f922e4075a5
riccardodivirgilio <riccardodivirgilio@gmail.com>
2024-04-29 14:37:58 +0200

    minor tweaks

--- PythonFunctions/Python/Test/validation.py
+++ PythonFunctions/Python/Test/validation.py
@@ -2,25 +2,19 @@ import datetime
 
 import numpy as np
 
+from typing import List
+from typing_extensions import TypedDict
 
-def repeat(Main: str, Count: int = 1, *, Separator: str = "") -> str:
+def Repeat(Main: str, Count: int = 1, *, Separator: str = "") -> str:
     return Separator.join(Main for _ in range(Count))
 
-
-def array_reshape(Array: np.ndarray, Dimension1: int, Dimension2: int) -> np.ndarray:
+def ArrayReshape(Array: np.ndarray, Dimension1: int, Dimension2: int) -> np.ndarray:
     return Array.reshape(Dimension1, Dimension2)
 
-
-from typing import List
-
-
-def dates_add(
+def DatesAdd(
     Dates: List[datetime.datetime], Delta: datetime.timedelta = 1
 ) -> List[datetime.datetime]:
-    return tuple(d + Delta for d in Dates)
-
-
-from typing_extensions import TypedDict
+    return [d + Delta for d in Dates]
 
 
 class Movie(TypedDict):
@@ -30,9 +24,7 @@ class Movie(TypedDict):
 
 class Information(TypedDict):
     Rating: int
+    Voters: int
 
-
-# This function expects two keyword arguments - `name` of type `str`
-# and `year` of type `int`.
-def movie_rating(movie: Movie) -> Information:
-    return Information(Rating=movie["Year"] % 10)
+def MovieRating(movie: Movie) -> Information:
+    return Information(Rating=movie["Year"] % 10, Voters=movie["Year"] * 2000)
