dc40073bfeef8a65ec08321224a51dda10577d6e
riccardodivirgilio <riccardodivirgilio@gmail.com>
2024-04-29 15:59:12 +0200

    Experimental automatic listability

--- PythonFunctions/Python/Common/function_handler.py
+++ PythonFunctions/Python/Common/function_handler.py
@@ -2,6 +2,47 @@ from wolframclient.utils.importutils import import_string
 from wolframclient.utils.importutils import (
     safe_import_string_and_call as import_string_and_call,
 )
+from wolframclient.language import wl
+from functools import partial
+
+def run_validation(func, *args, **opts):
+
+    from pydantic import ValidationError
+
+    try:
+        return func(*args, **opts)
+    except ValidationError as exception:
+
+        errors = exception.errors()
+
+        for err in errors:
+            #dropping pydantic urls
+            err.pop('url')
+
+        # Experimental automatic listability
+
+        if len(errors) == 1 and errors[0]['loc'] == (0, ) and not errors[0]['type'] == 'list_type':
+
+            if isinstance(args[0], (tuple, list)):
+                return tuple(
+                    run_validation(func, p, *args[1:], **opts)
+                    for p in args[0]
+                )
+            if isinstance(args[0], dict):
+                return {
+                    k: run_validation(func, p, *args[1:], **opts)
+                    for k, p in args[0].items()
+                }
+
+        return wl.Failure(
+            "InterpretationFailure", {
+                #'MessageTemplate': "\n".join(err['msg'] for err in errors),
+                'MessageTemplate': str(exception),
+                'MessageParameters': {},
+                'Errors': errors
+            }
+        )
+
 
 
 def function_handler(path, validate_call=True):
@@ -18,6 +59,8 @@ def function_handler(path, validate_call=True):
                 "arbitrary_types_allowed": True, 
                 "strict": False, 
             },
-        ) 
+        )
+
+        func = partial(run_validation, func)
 
     return func
