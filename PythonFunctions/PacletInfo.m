fd4202326bd993058f6b8c95b7ab4eccf30f0d9e
riccardodivirgilio <riccardodivirgilio@gmail.com>
2024-08-13 09:09:20 +0200

    dropping extensions from the lib

--- PythonFunctions/PacletInfo.m
+++ PythonFunctions/PacletInfo.m
@@ -13,41 +13,6 @@ Paclet[
             "Root" -> ".", 
 			"Context" -> {"PythonFunctions`"}, 
 			"Symbols" -> {}
-		},
-		{
-			"PythonFunctions",
-			"Namespace" -> "Virtual",
-			"Functions" -> {
-				"Datetime"   -> {"datetime", "datetime"},
-				"Date"       -> {"datetime", "date"},
-				"Now"        -> {"datetime", "datetime", "now"},
-				"Today"      -> {"datetime", "date", "today"}
-			},
-			"Evaluator" -> <|
-				"Dependencies" -> {"pydantic"}
-			|>,
-			"Validate" -> False
-		},
-		{
-			"PythonFunctions",
-			"Namespace" -> "Code",
-			"Root" -> "Python/Functions/AnotherEnv"
-		},
-		{
-			"PythonFunctions",
-			"Namespace" -> "Science",
-			"Root" -> "Python/Functions/Science",
-			"Evaluator" -> <|
-				"Dependencies" -> {"lightcurve", "pydantic"}
-			|>
-		},
-		{
-			"PythonFunctions",
-			"Namespace" -> "Code",
-			"Root" -> "Python/Functions/Code",
-			"Evaluator" -> <|
-				"Dependencies" -> {"lightcurve", "pydantic"}
-			|>
 		}
 	}
 ]
